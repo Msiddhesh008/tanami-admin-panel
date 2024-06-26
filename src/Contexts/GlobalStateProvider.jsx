e958adcc3de35993e87f8e62aa8fa2dd9d9ed4b7
rockyeverlast <rockyeverlast@gmail.com>
2024-06-26 13:59:29 +0530

    Investor details page update

--- src/Contexts/GlobalStateProvider.jsx
+++ src/Contexts/GlobalStateProvider.jsx
@@ -4,12 +4,12 @@ import GlobalStateContext from "./GlobalStateContext";
 import { effect, useColorMode } from "@chakra-ui/react";
 import { v4 as uuidv4 } from "uuid";
 
-
 const getRandomDate = (start, end) => {
-  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  const date = new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
   return date.toISOString();
 };
-
 
 const startDate = new Date(2020, 0, 1); // January 1, 2020
 const endDate = new Date(); // Current date
@@ -276,18 +276,8 @@ const GlobalStateProvider = ({ children }) => {
       targClose: "24 December",
       holdingPer: "5-7 years",
       progressValue: 80,
-    },{
-      imgSrc:
-        "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
-      title: "Multi Family Residence Portfolio",
-      sponsor: "KKR",
-      annReturn: "12.5%",
-      annYield: "12.5%",
-      minInvests: ",00 $0",
-      targClose: "24 December",
-      holdingPer: "5-7 years",
-      progressValue: 80,
-    },{
+    },
+    {
       imgSrc:
         "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
       title: "Multi Family Residence Portfolio",
@@ -299,114 +289,137 @@ const GlobalStateProvider = ({ children }) => {
       holdingPer: "5-7 years",
       progressValue: 80,
     },
-  ])
-
-  const [ rateExchange, setRateExchange ] = useState([
     {
-      id : uuidv4(),
+      imgSrc:
+        "https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60",
+      title: "Multi Family Residence Portfolio",
+      sponsor: "KKR",
+      annReturn: "12.5%",
+      annYield: "12.5%",
+      minInvests: ",00 $0",
+      targClose: "24 December",
+      holdingPer: "5-7 years",
+      progressValue: 80,
+    },
+  ]);
+
+  const [rateExchange, setRateExchange] = useState([
+    {
+      id: uuidv4(),
       fromCurr: "USD $",
       toCurr: "BHD .د.ب;",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "USD $",
       toCurr: "KWD ·د.ك",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "USD $",
       toCurr: "OMR ﷼.",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "USD $",
       toCurr: "QAR ﷼.",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "USD $",
       toCurr: "SAR ﷼.",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "USD $",
       toCurr: "AED د.إ. ",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
 
-
-
-
-
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "AED د.إ. ",
       toCurr: "USD $",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "SAR ﷼.",
       toCurr: "USD $",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "QAR ﷼.",
       toCurr: "USD $",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
 
-
-    
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "BHD .د.ب;",
       toCurr: "USD $",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "KWD ·د.ك",
       toCurr: "USD $",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
     {
-      id : uuidv4(),
+      id: uuidv4(),
       fromCurr: "OMR ﷼.",
       toCurr: "USD $",
       effectFrom: getRandomDate(startDate, endDate),
       effectTill: getRandomDate(startDate, endDate),
-      rate: 2.66
+      rate: 2.66,
     },
-  ])
+  ]);
+
+  const [InvestorDetails, setInvestorDetails] = useState([
+    {
+      id: 1,
+      InvestorName: "ICC",
+      Sponsor: "Adani",
+      InvestmentAmount: "1000000",
+      Action: "Distribute",
+    },
+    {
+      id: 2,
+      InvestorName: "ECHO Investment",
+      Sponsor: "Tata",
+      InvestmentAmount: "2500000",
+      Action: "Distribute",
+    },
+  ]);
 
   return (
     <GlobalStateContext.Provider
@@ -423,7 +436,12 @@ const GlobalStateProvider = ({ children }) => {
         toggleColorMode,
         slideFromRight,
         setSlideFormRight,
-        investment, setInvestment, rateExchange, setRateExchange
+        investment,
+        setInvestment,
+        rateExchange,
+        setRateExchange,
+        InvestorDetails,
+        setInvestorDetails,
       }}
     >
       {children}
